5d353c78f5ec8a567d34b3cdb06188143be8694a
Manav Manish Parmar <mmparmar044@gmail.com>
2024-08-14 07:58:14 +0530

    SignUp and SignIn UI updated

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,7 +1,48 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <div className="flex items-center justify-center">
-    <SignIn afterSignOutUrl="/"/>
-  </div>;
+  return <div className="">
+    {/*
+  Heads up! 👋
+
+  Plugins:
+    - @tailwindcss/forms
+*/}
+
+<section className="bg-white">
+  <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
+    <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
+      <img
+        alt=""
+        src="https://images.pexels.com/photos/4871011/pexels-photo-4871011.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        className="absolute inset-0 h-full w-full object-cover opacity-80"
+      />
+
+      <div className="hidden lg:relative lg:block lg:p-12">
+        <a className="block text-white" href="#">
+          <span className="sr-only">Home</span>
+        </a>
+
+        <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
+          Welcome to Attendance Monitoring system
+        </h2>
+
+        <p className="mt-4 leading-relaxed text-white/90">
+        The hasslefree solution for recording the attendance of the students.
+        </p>
+      </div>
+    </section>
+
+    <main
+      className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6"
+    >
+      <SignIn afterSignOutUrl="/"/>
+    </main>
+  </div>
+</section>
+  </div>
 }
+
+{/* <div className="flex items-center justify-center">
+    <SignIn afterSignOutUrl="/"/>
+  </div>; */}
--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,18 +1,26 @@
-"use client"
-import { useRouter } from 'next/navigation'
-import React from 'react'
+"use client";
+import { useRouter } from "next/navigation";
+import React from "react";
 
 function Header() {
-  const router = useRouter()
+  const router = useRouter();
   const handleGetStarted = () => {
-    router.push("/dashboard")
-  }
+    router.push("/dashboard");
+  };
+
   return (
-    <div className='p-4 flex justify-between items-center w-full pl-20 pr-20 fixed bg-white'>
-        <h1 className='text-2xl font-semibold text-primary pt-2'>Attendance Monitoring</h1>
-        <button onClick={handleGetStarted} className='px-4 py-2 bg-primary text-white rounded-sm'>Get Started</button>
+    <div className="p-8 flex justify-between items-center w-full bg-white fixed z-10">
+      <h1 className="text-xl sm:text-2xl font-semibold text-primary uppercase">
+        Attendance Monitoring
+      </h1>
+      <button
+        onClick={handleGetStarted}
+        className="px-3 py-1 sm:px-4 sm:py-2 bg-primary text-white rounded-sm"
+      >
+        Get Started
+      </button>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
